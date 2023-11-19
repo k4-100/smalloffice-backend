8b50c9616490a3b417a9785ff9fd05986d2054cb
k4-100 <k4-100@protonmail.com>
2023-11-19 20:26:18 +0100

    changed limit for json files transfered

---
+++ run_docker.sh
@@ -0,0 +1,1 @@
+docker run --name smalloffice-backend-1 -p 5000:5000 -d smalloffice-backend
--- src/index.ts
+++ src/index.ts
@@ -31,7 +31,7 @@ app.use(express.urlencoded({ extended: false }));
 
 // parse application/json
 // app.use(express.json());
-app.use(express.json({ limit: "5mb" }));
+app.use(express.json({ limit: "2mb" }));
 // parse cookies
 app.use(cookieParser());
 
